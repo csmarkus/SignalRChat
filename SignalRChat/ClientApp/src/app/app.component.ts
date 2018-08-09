5d5b78aaa3f631b66fa23f67d404268cb291809c
Unknown <infected.shadow0@gmail.com>
2018-08-08 22:19:51 -0400

    Updated app component to start interacting with signalr hub

--- SignalRChat/ClientApp/src/app/app.component.ts
+++ SignalRChat/ClientApp/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'ClientApp';
+export class AppComponent implements OnInit {
+    title = 'ClientApp';
+
+    public hubConnection: HubConnection;
+    public messages: string[] = [];
+    public message: string;
+
+    ngOnInit() {
+        let builder = new HubConnectionBuilder();
+        this.hubConnection = builder.withUrl('/chathub').build();
+
+        this.hubConnection.on("ReceiveMessage", (message) => {
+            this.messages.push(message);
+        });
+
+        this.hubConnection.start();
+    }
+
+    SendMessage() {
+        this.hubConnection.invoke("SendMessage", this.message);
+        this.message = "";
+    }
 }
